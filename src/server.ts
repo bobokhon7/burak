790015cc94abd47d3d1ba4c87e302d164a5b8882
Bobokhon <rboboxon6055@gmail.com>
2024-02-24 04:19:31 +0900

    feat: connect MongoDb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("mongoDb connection succed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("Error on connection Mongodb", err));
