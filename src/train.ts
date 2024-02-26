e62fb95ed79a14206fcf57e5c51067b0a80c7180
Bobokhon <rboboxon6055@gmail.com>
2024-02-26 16:38:29 +0900

    H task solved

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,14 @@
+// /*H-TASK:
+
+// Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
+// MASALAN: gerReverse("hello") return qiladi "olleh"
+
+const gerReverse = (str: string) => {
+  return str.split("").reverse().join("");
+};
+
+console.log(gerReverse("hello"));
+
 // /*G-TASK:
 
 // Shunday function tuzingki unga integerlardan iborat array pass
@@ -5,9 +16,9 @@
 // tegishli birinchi indexni qaytarsin.
 // MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
-const getHighestIndex = (num: number[]) => {
-  let getHigestNum = Math.max(...num);
-  return num.indexOf(getHigestNum);
-};
+// const getHighestIndex = (num: number[]) => {
+//   let getHigestNum = Math.max(...num);
+//   return num.indexOf(getHigestNum);
+// };
 
-console.log(getHighestIndex([5, 21, 12, 21, 8]));
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
