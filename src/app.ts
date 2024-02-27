76bb14698f0b0011513fdcb2c4cec421c5aa5508
Bobokhon <rboboxon6055@gmail.com>
2024-02-27 14:39:23 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /**   1-ENTRANCE  **/
 const app = express();
@@ -13,5 +14,6 @@ app.set("view", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /**   $-ROUTES  **/
+app.use("/", router); //Middleware Design Pattern
 
 export default app;
